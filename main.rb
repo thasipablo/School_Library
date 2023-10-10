49bd81bf40c1af6a908a94afe08fe5be08172ae5
thasipablo <thasipablo@gmail.com>
2023-10-10 17:53:19 +0100

    add nameable class

--- main.rb
+++ main.rb
@@ -5,8 +5,9 @@ student = Student.new(9, 'En', 'Vicky')
 puts student.name
 student.name = 'Gloire'
 puts student.name
-puts student.can_use_services
+puts student.can_use_services?
+puts "Correct name #{student.correct_name}"
 
 teacher = Teacher.new(34, 'Machine Learning', 'Dry')
 puts teacher.name
-puts teacher.can_use_services
+puts teacher.can_use_services?
