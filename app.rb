0354396349a386763da85688ae0e81193a9a425b
unknown <macherki.nessrine@yahoo.com>
2023-10-17 07:53:50 +0100

    update reade me file

--- app.rb
+++ app.rb
@@ -4,119 +4,82 @@ require './lib/book'
 require './lib/rental'
 
 module Library
-  # Warnig method that displays a wrong message passed
-  def wrong_number_msg
-    puts "\n*** [WARNING] You passed a wrong number ***"
-    puts "\n"
+  # Display a warning message
+  def warn(message)
+    puts "\n*** [WARNING] #{message} ***\n\n"
   end
 
-  # method that checks whether option number is valid or not
-  def valid_number?(range, choice)
-    unless range.include?(choice)
-      wrong_number_msg
-      return false
-    end
-    true
+  # Display a success message
+  def success(message)
+    puts "\n#{message} Created successfully\n\n"
   end
 
-  # Method that displays customized success message
-  def success_msg(label)
-    puts "\n#{label} Created successfully"
-    puts "\n"
-  end
-
-  # Method that displays all recorded books
-  def get_list_books(list_book, show_index)
-    puts "\nAll Books:"
-    if list_book.length.positive?
-      list_book.each.with_index do |book, index|
-        book_index = show_index ? "#{index})" : ''
-        puts "#{book_index} Title: #{book.title},  Author: #{book.author}"
-      end
-    else
+  # Display a list of items
+  def list_items(items, item_type)
+    puts "\nAll #{item_type}:"
+    if items.empty?
       puts 'No record found'
-    end
-    puts "\n"
-  end
-
-  # Method that displays all recorded person
-  def get_list_person(list_person, show_index)
-    puts "\nAll People:"
-    if list_person.length.positive?
-      list_person.each.with_index do |people, index|
-        person_index = show_index ? "#{index})" : ''
-        puts "#{person_index} #{people.description}"
-      end
     else
-      puts 'No record found'
+      items.each.with_index do |item, index|
+        puts "#{index}) #{item.description}"
+      end
     end
     puts "\n"
   end
 
-  # Method that displays all user rental
-  def get_user_rental(list_person)
-    puts 'All rentals for a given person id'
-    id = gets.chomp
-    list_person.each do |person|
-      person.rentals_description if person.id == id.to_i
-    end
-    puts "\n"
+  # Get user input
+  def get_input(prompt)
+    print "#{prompt}: "
+    gets.chomp
   end
 
-  # Method that helps to add new person to the array
-  def add_new_person(list_person, choice)
-    return unless valid_number?(%w[1 2], choice)
+  # Add a new person to the list
+  def add_person(list, type)
+    age = get_input('Age')
+    name = get_input('Name')
 
-    print 'Age: '
-    age = gets.chomp
-    print 'Name: '
-    name = gets.chomp
-    case choice
-    when '1'
-      student = Student.new(nil, age, name)
-      print 'Has Parent Permission? [Y/N]: '
-      par_permission = gets.chomp
-      student.parent_permission = (par_permission.upcase == 'Y')
-      list_person.push(student)
-    when '2'
-      teacher = Teacher.new(nil, age, name)
-      print 'Specialization: '
-      teacher.specialization = gets.chomp
-      list_person.push(teacher)
+    if type == 'Student'
+      permission = get_input('Has Parent Permission? [Y/N]').upcase == 'Y'
+      person = Student.new(nil, age, name)
+      person.parent_permission = permission
+    else
+      specialization = get_input('Specialization')
+      person = Teacher.new(nil, age, name)
+      person.specialization = specialization
     end
-    success_msg('Person')
+
+    list.push(person)
+    success('Person')
   end
 
-  # Method that helps to add new book to the array
-  # list_book: array of books
-  def add_new_book(list_book)
+  # Add a new book to the list
+  def add_book(list)
+    title = get_input('Title')
+    author = get_input('Author')
+
     book = Book.new(nil, nil)
-    print 'title: '
-    book.title = gets.chomp
-    print 'Author: '
-    book.author = gets.chomp
-    list_book.push(book)
-    success_msg('Book')
+    book.title = title
+    book.author = author
+
+    list.push(book)
+    success('Book')
   end
 
-  # Method to create a rental
-  def create_new_rental(list_rental, list_book, list_person)
-    puts 'Select a book from the following list by number:'
-    get_list_books(list_book, true)
-    book_index = gets.chomp.to_i
+  # Create a rental
+  def create_rental(rentals, books, people)
+    list_items(books, 'Books')
+    book_index = get_input('Select a book by number').to_i
 
-    puts 'Select a person from the following list by number (not id):'
-    get_list_person(list_person, true)
-    person_index = gets.chomp.to_i
+    list_items(people, 'People')
+    person_index = get_input('Select a person by number').to_i
 
-    if person_index < list_person.length && book_index < list_book.length
-      print 'Date: '
-      rental_date = gets.chomp
-      rental = Rental.new(rental_date, list_book[book_index], list_person[person_index])
-      list_rental.push(rental)
-      success_msg('Rental')
+    if person_index < people.length && book_index < books.length
+      rental_date = get_input('Date')
+      rental = Rental.new(rental_date, books[book_index], people[person_index])
+      rentals.push(rental)
+      success('Rental')
     else
-      wrong_number_msg
+      warn('Invalid book or person number')
     end
   end
 end
