0b960336ffd2ab0615b9588dfe7f10186a23446c
thasipablo <thasipablo@gmail.com>
2023-10-20 14:57:39 +0100

    add test to person spec

--- tests/person_spec.rb
+++ tests/person_spec.rb
@@ -7,6 +7,15 @@ describe Person do
   let(:person) { Person.new(25, 'John', parent_permission: true) }
   let(:book) { Book.new('Sample Book', 'John Doe') }
 
+  describe 'initialization' do
+    it 'initializes a person with the provided age, name, and parent permission' do
+      expect(person.age).to eq(25)
+      expect(person.name).to eq('John')
+      expect(person.parent_permission).to be(true)
+      expect(person.id).to be_between(1, 1000)
+    end
+  end
+
   describe '#can_use_services?' do
     it 'returns true when the person is of age' do
       person.age = 21
@@ -47,6 +56,23 @@ describe Person do
 
     it 'prints "No record found" when there are no rentals' do
       expect { person.rentals_description }.to output("No record found\n").to_stdout
+    end
+  end
+
+  describe '#filter_user_rentals' do
+    it 'filters rentals for a specific person' do
+      book1 = Book.new('Dev', 'Louis')
+      book2 = Book.new('The glossy ruby', 'Violet')
+      book3 = Book.new('Once upon a time', 'Claude')
+
+      rental1 = Rental.new('2023-10-20', book1, person)
+      rental2 = Rental.new('2023-10-21', book2, person)
+      rental3 = Rental.new('2023-10-22', book3, Person.new(12, 'Odette'))
+
+      rentals = [rental1, rental2, rental3]
+      filtered_rentals = person.filter_user_rentals(rentals, person.id)
+
+      expect(filtered_rentals).not_to include(rental3)
     end
   end
 end
